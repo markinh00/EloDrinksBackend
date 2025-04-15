fd76587968cc6cacf76429e37a134853d50050a8
Mathz <mathlimapereira@gmail.com>
2025-04-15 15:48:35 -0300

    fix: allow cors

--- api/api.py
+++ api/api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import bar_structure
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(bar_structure.router)
 
