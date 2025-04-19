7225cb3db218fcfd68a8374a77fe0d44ae7d1d2d
João Marcos Coutinho Nunes <jmcoutinhonunes@gmail.com>
2025-04-19 18:28:08 -0300

    faet: add auth to structure route

--- api/routes/bar_structure.py
+++ api/routes/bar_structure.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Query
 from typing import List
-
+from fastapi.params import Security
+from api.dependencies.auth import get_current_user
+from api.schemas.user import UserScopes
 from api.services.bar_structure import BarStructureService
 from api.schemas.bar_structure import (
     BarStructureCreate,
@@ -12,17 +14,17 @@ router = APIRouter(prefix="/structure", tags=["Structure"])
 service = BarStructureService()
 
 
-@router.post("/", response_model=BarStructureRead)
+@router.post("/", response_model=BarStructureRead, dependencies=[Security(get_current_user, scopes=[UserScopes.ADMIN.value])])
 def create_bar_structure(bar_data: BarStructureCreate):
     return service.create_bar(name=bar_data.name, price=bar_data.price)
 
 
-@router.get("/", response_model=List[BarStructureRead])
+@router.get("/", response_model=List[BarStructureRead], dependencies=[Security(get_current_user, scopes=[UserScopes.ADMIN.value, UserScopes.CUSTOMER.value])])
 def get_all_bar_structures(page: int = Query(1, ge=1), size: int = Query(10, ge=1, le=100)):
     return service.get_all_bars(page=page, size=size)
 
 
-@router.get("/{bar_id}", response_model=BarStructureRead)
+@router.get("/{bar_id}", response_model=BarStructureRead, dependencies=[Security(get_current_user, scopes=[UserScopes.ADMIN.value, UserScopes.CUSTOMER.value])])
 def get_bar_structure(bar_id: int):
     bar = service.get_bar_by_id(bar_id)
     if not bar:
@@ -30,7 +32,7 @@ def get_bar_structure(bar_id: int):
     return bar
 
 
-@router.put("/{bar_id}", response_model=BarStructureRead)
+@router.put("/{bar_id}", response_model=BarStructureRead, dependencies=[Security(get_current_user, scopes=[UserScopes.ADMIN.value])])
 def update_bar_structure(bar_id: int, updated_data: BarStructureUpdate):
     updated = service.update_bar(bar_id, updated_data.model_dump())
     if not updated:
@@ -38,7 +40,7 @@ def update_bar_structure(bar_id: int, updated_data: BarStructureUpdate):
     return updated
 
 
-@router.delete("/{bar_id}")
+@router.delete("/{bar_id}", dependencies=[Security(get_current_user, scopes=[UserScopes.ADMIN.value])])
 def delete_bar_structure(bar_id: int):
     success = service.delete_bar(bar_id)
     if not success:
