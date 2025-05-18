f213744c6da5be3c00151a306e01e9d47f30bfe5
Mathz <mathlimapereira@gmail.com>
2025-05-18 15:59:11 -0300

    monkey test vercel 🐒

--- api/services/db/mongodb/mongo_connection.py
+++ api/services/db/mongodb/mongo_connection.py
@@ -6,17 +6,32 @@ from motor.motor_asyncio import (
     AsyncIOMotorCollection,
     AsyncIOMotorDatabase,
 )
+import asyncio
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DATABASE = os.getenv("MONGO_DATABASE")
 
-client = AsyncIOMotorClient(MONGO_URI)
+_client = None
 
 
-def get_orders_collection() -> AsyncIOMotorCollection:
+def get_mongo_client():
+    global _client
+    if _client is None:
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+
+        _client = AsyncIOMotorClient(MONGO_URI)
+    return _client
+
+
+async def get_orders_collection() -> AsyncIOMotorCollection:
     try:
+        client = get_mongo_client()
         db: AsyncIOMotorDatabase = client[MONGO_DATABASE]
         collection: AsyncIOMotorCollection = db["orders"]
         logging.info("Asynchronous connection successfully established.")
@@ -24,6 +39,3 @@ def get_orders_collection() -> AsyncIOMotorCollection:
     except Exception as e:
         logging.error(f"Error connecting to the database: {e}")
         raise
-
-
-# asyncio é foda
